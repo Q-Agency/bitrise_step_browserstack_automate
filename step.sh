b7462414650bce7b6804bc876827d1c21ccf7012
zlatkomq <99484503+zlatkomq@users.noreply.github.com>
2022-02-24 10:18:05 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 set -ex
 
-app_url=$(curl -u "${browserstack_user_name}:${browserstack_access_key}" -X POST "https://api-cloud.browserstack.com/app-automate/upload" -F "file=@$file_to_upload_apk")
+app_url_apk=$(curl -u "${browserstack_user_name}:${browserstack_access_key}" -X POST "https://api-cloud.browserstack.com/app-automate/upload" -F "file=@$file_to_upload_apk")
 
 echo $app_url
   if [[ "$app_url" == *"app_url"* ]]
   then
-    app_url=`echo $app_url | awk -F':' '{ print $2 ":" $3 }' | awk '{ print substr( $0, 2, length($0)-3 ) }'`
-    #app_url=`awk '{ print substr( $0, 1, length($0)-1 ) }'`
-    #app_url=${app_url%??}
-    echo $app_url
-    envman add --key BROWSERSTACK_APP_AUTOMATE_APK_URL --value "$app_url"
+    app_url_apk=`echo $app_url_apk | awk -F':' '{ print $2 ":" $3 }' | awk '{ print substr( $0, 2, length($0)-3 ) }'`
+    echo $app_url_apk
+    envman add --key BROWSERSTACK_APP_AUTOMATE_APK_URL --value "$app_url_apk"
     exit 0
   else
-    echo $app_url
+    echo $app_url_apk
     echo "Error uploading file to Browserstack"
     exit 1
   fi
