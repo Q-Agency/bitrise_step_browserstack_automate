f2f07cd912967e629f03950c98cb902fda22b0c0
zlatkomq <99484503+zlatkomq@users.noreply.github.com>
2022-02-25 12:27:34 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -ex
 
-app_url_apk=$(curl -u "${browserstack_user_name}:${browserstack_access_key}" -X POST "https://api-cloud.browserstack.com/app-automate/upload" -F "file=@$file_to_upload_apk" -F "custom_id=${custom_id}")
+app_url=$(curl -u "mobile58:VXKExFmpqsSfyoNSXFy7" -X POST "https://api-cloud.browserstack.com/app-automate/upload" -F "file=@$file_to_upload_apk" -F "custom_id=${custom_id}")
 
 echo $app_url
   if [[ "$app_url" == *"app_url"* ]]
   then
-    app_url_apk=`echo $app_url_apk | awk -F':' '{ print $2 ":" $3 }' | awk '{ print substr( $0, 2, length($0)-3 ) }'`
-    echo $app_url_apk
-    envman add --key BROWSERSTACK_APP_AUTOMATE_APK_URL --value "$app_url_apk"
+    app_url=`echo $app_url | awk -F':' '{ print $2 ":" $3 }' | awk '{ print substr( $0, 2, length($0)-3 ) }'`
+    echo $app_url
+    envman add --key BROWSERSTACK_APP_AUTOMATE_APK_URL --value "$app_url"
     exit 0
   else
-    echo $app_url_apk
+    echo $app_url
     echo "Error uploading file to Browserstack"
     exit 1
   fi
