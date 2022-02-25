bb7597743d397cc9d5398701987179b852ce36e1
zlatkomq <99484503+zlatkomq@users.noreply.github.com>
2022-02-25 14:24:11 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -ex
 
-app_url=$(curl -u "${browserstack_user_name}:${browserstack_access_key}" -X POST "https://api-cloud.browserstack.com/app-automate/upload" -F "file=@$file_to_upload_apk" -F "custom_id=${custom_id}")
+app_url=$(curl -u "$browserstack_username:$browserstack_access_key" -X POST "https://api-cloud.browserstack.com/app-automate/upload" -F "file=@$file_to_upload_apk" -F "custom_id=${custom_id}")
 
 echo $app_url
   if [[ "$app_url" == *"app_url"* ]]
   then
-    app_url=`echo $app_url | awk -F':' '{ print $2 ":" $3 }' | awk '{ print substr( $0, 2, length($0)-3 ) }'`
+    app_url=`echo $app_url | awk -F':' '{ print $2 ":" $3 }' | awk '{ print substr( $0, 2, length($0)-12 ) }'`
     echo $app_url
     envman add --key BROWSERSTACK_APP_AUTOMATE_APK_URL --value "$app_url"
     exit 0
